b7c6fb705c20ddb4129a7037f72db71ea548650a
superlee <772608796@qq.com>
2023-04-05 13:39:20 +0800

    配置调整

--- dice-server/sql/init.sql
+++ dice-server/sql/init.sql
@@ -333,7 +333,7 @@ create table article
     comment_count int        default 0 not null comment '评论数量',
     priority      int        default 0 null comment '文章优先级',
     create_time   timestamp            not null comment '创建时间',
-    update_time   timestamp            not null on update CURRENT_TIMESTAMP comment '更新时间',
+    update_time   timestamp            not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP comment '更新时间',
     creator       int                  null comment '创建人',
     modifier      int                  null comment '修改人',
     deleted       int        default 0 null comment '逻辑删除标识(0.未删除,1.已删除)'
